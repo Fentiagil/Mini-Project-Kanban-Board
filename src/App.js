37d1f25f859d771b4fe661455219277d70d95128
Fentiagil <fentiagil@gmail.com>
2024-05-09 11:37:52 +0700

    menambahkan header kanban app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Header */}
+      <header className="App-header"> 
+        <div className='Header-menu'>      
+          <div className="Right-side">
+            <div className="Heading">
+              <h3 className="Header-brand">Product Roadmap</h3>
+            </div>
+            <div className="Button flex-container">
+              <button type="button" className="btn-newgroup">+ Add New Group</button>
+            </div>
+            
+          </div>
+
+          <div className="Left-side"></div>
+        </div>      
+      </header><hr></hr>
+
+      {/* Body */}
+      <div className="App-body">
+      
+      </div>
+
     </div>
   );
 }
